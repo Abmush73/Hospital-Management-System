e65b981ef6d441c5912fa775d7eb1cdc83ce64e5
Abdulbaqi Mustapha Shehu <abdoulmshehu@gmail.com>
2026-09-13 00:33:23 +0100

    Added search patient feature

--- Main.py
+++ Main.py
@@ -29,11 +29,27 @@ def handle_choice(choice):
                 Age: {patient ["age"]}
                 Gender: {patient ["gender"]}
                 """)
+     elif choice == 3:
+         search_name = input("Enter patient's Name to Search: ")
+         found = False
+         for patient in patients_list:
+              if patient["name"] == search_name:
+                 print(f"""
+                 patient
+                 -------
+                 Name: {patient ["name"]}
+                 Age: {patient ["age"]}
+                 Gendeer: {patient ["gender"]}
+                 """)
+                 found = True
+                 break
+         if not found:
+             print("patient not found")
+             
      else:
          print("Invalid Option")
-
-print("Select From The Available Options")
 while True:
+   print("Select From The Available Options")
    print("""
     =================================
     HOSPITAL MANAGEMENT SYSTEM
@@ -41,12 +57,13 @@ while True:
 
     1. Register Patient
     2. View Patients
-    3. Exit
+    3. Search Patients
+    4. Exit
     """)
    try:
     choice = int(input("Enter your choice: "))
     handle_choice(choice) 
-    if choice == 3:
+    if choice == 4:
         print("Thanks for using the system")
         break
    except ValueError:
